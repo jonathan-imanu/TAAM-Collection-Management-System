3145676d101fdde80595663f02564c84e6437aac
Joshua Andres <joshuaandres@jcs-mac.home>
2024-07-19 08:22:07 -0400

    feat: added uniqueness to lot field and spinners to add

--- app/src/main/java/com/taam/collection_management_system/AddItemFragment.java
+++ app/src/main/java/com/taam/collection_management_system/AddItemFragment.java
@@ -12,13 +12,15 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.google.firebase.database.*;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class AddItemFragment extends Fragment {
-    private EditText editTextName, editTextLot, editTextCategory, editTextPeriod, editTextDescription;
+    private EditText editTextName, editTextLot, editTextDescription;
     private Spinner spinnerCategory;
+    private Spinner spinnerPeriod;
     private Button buttonAdd;
 
     private FirebaseDatabase db;
@@ -29,12 +31,11 @@ public class AddItemFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_item, container, false);
 
-        editTextName= view.findViewById(R.id.editTextName);
+        editTextName = view.findViewById(R.id.editTextName);
         editTextLot = view.findViewById(R.id.editTextLot);
-        editTextCategory = view.findViewById(R.id.editTextCategory);
-        editTextPeriod = view.findViewById(R.id.editTextPeriod);
         editTextDescription = view.findViewById(R.id.editTextDescription);
         spinnerCategory = view.findViewById(R.id.spinnerCategory);
+        spinnerPeriod = view.findViewById(R.id.spinnerPeriod);
         buttonAdd = view.findViewById(R.id.buttonAdd);
 
         db = FirebaseDatabase.getInstance("https://taam-management-system-default-rtdb.firebaseio.com/");
@@ -51,6 +52,19 @@ public class AddItemFragment extends Fragment {
             }
         });
 
+        // Set up the spinner with periods
+        ArrayAdapter<CharSequence> adapter_p = ArrayAdapter.createFromResource(getContext(),
+                R.array.period_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPeriod.setAdapter(adapter_p);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+        });
+
+
         return view;
     }
 
@@ -58,28 +72,40 @@ public class AddItemFragment extends Fragment {
         String name = editTextName.getText().toString().trim();
         String lot = editTextLot.getText().toString().trim();
         String description = editTextDescription.getText().toString().trim();
-        String category = editTextCategory.getText().toString().trim();
-        String period = editTextPeriod.getText().toString().trim();
-        //String category = spinnerCategory.getSelectedItem().toString().toLowerCase();
-        //String period = spinnerCategory.getSelectedItem().toString().toLowerCase();
-        //Upload file here, maybe a link?
+        String category = spinnerCategory.getSelectedItem().toString().toLowerCase();
+        String period = spinnerPeriod.getSelectedItem().toString().toLowerCase();
 
         if (name.isEmpty() || lot.isEmpty() || description.isEmpty()) {
             Toast.makeText(getContext(), "Please fill out all fields", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        DatabaseReference myRef = db.getReference();
-        //myRef.child("collection_data").child("1").child("Category").setValue("a");
-        itemsRef = db.getReference();
-        String id = itemsRef.push().getKey();
-        Item item = new Item(id, lot, name, category, period, description);
+        DatabaseReference myRef = db.getReference("/collection_data");
 
-        itemsRef.child(id).setValue(item).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                Toast.makeText(getContext(), "Item added", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(getContext(), "Failed to add item", Toast.LENGTH_SHORT).show();
+        myRef.orderByChild("lot").equalTo(lot).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    Toast.makeText(getContext(), "Lot number already exists.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                String id = myRef.push().getKey();
+                Item item = new Item(id, lot, name, category, period, description);
+
+                myRef.child(id).setValue(item).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getContext(), "Item added", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getContext(), "Failed to add item", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getContext(), "Error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
