c93080ea52d15393f0f7d967df51c78ecb9767da
Justin N <32967402+justj09@users.noreply.github.com>
2024-07-23 00:25:57 -0400

    feat: create checkBoxList to store check boxes from table

--- app/src/main/java/com/taam/collection_management_system/ItemAdapter.java
+++ app/src/main/java/com/taam/collection_management_system/ItemAdapter.java
@@ -4,15 +4,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.CheckBox;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
     private List<Item> itemList;
+    private List<CheckBox> checkBoxList;
 
-    public ItemAdapter(List<Item> itemList) {
+    public ItemAdapter(List<Item> itemList, List<CheckBox> checkBoxList) {
         this.itemList = itemList;
+        this.checkBoxList = checkBoxList;
     }
 
     @NonNull
@@ -31,6 +34,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
         holder.textViewCategory.setText(item.getCategory());
         holder.textViewPeriod.setText(item.getPeriod());
         holder.textViewDescription.setText(item.getDescription());
+        checkBoxList.add(holder.selectCheckBox);
     }
 
     @Override
@@ -41,6 +45,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
     public static class ItemViewHolder extends RecyclerView.ViewHolder {
         TextView textViewLot, textViewName, textViewCategory, textViewPeriod,
                 textViewDescription;
+        CheckBox selectCheckBox;
 
         public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -49,6 +54,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
             textViewCategory = itemView.findViewById(R.id.textViewCategory); //change to field
             textViewPeriod = itemView.findViewById(R.id.textViewPeriod);
             textViewDescription = itemView.findViewById(R.id.textViewDescription);
+            selectCheckBox = itemView.findViewById(R.id.selectCheckBox);
         }
     }
 }
